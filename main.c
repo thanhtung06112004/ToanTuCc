28ce4428d1584e1c9600ee74c1ff3561cba9d8f8
thanhtung06112004 <145945848+thanhtung06112004@users.noreply.github.com>
2023-11-01 07:25:11 +0700

    Add files via upload

---
+++ main.c
@@ -0,0 +1,45 @@
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    
+//    int a, b;
+//    float Tong, Hieu, Tich, Thuong;
+//    
+//    // nhap 2 so nguyen);
+//    scanf("%d", &a);
+//    printf("Nhap vao so nguyen b: ");
+//    scanf("%d", &b);
+//    
+//    // tinh toan tong, hieu, tich va thuongso a, b
+//    Tong = a + b;
+//    Hieu = a - b;
+//    Tich = a * b;
+//    Thuong = (float)a / b;
+//    
+//    // in ket qua ra man hinh
+//    printf("Tong c   printf("Hieu cua a va b la: %.2f\n", Hieu);
+//    printf("Tich cua a va b la: %.2f\n", Tich);
+//  printf("Thuong cua ab la: %.2f\n", Thuong);
+    
+//    THUC THI CAC TOAN TU SO SANH
+//    int a, b, c;
+//       
+//       printf("Nhap vao so nguyen a: ");
+//       scanf("%d", &a);
+//       printf("Nhap vao so nguyen b: ");
+//       scanf("%d", &b);
+//       
+//       c = (a > b);
+//       printf("a > b: %d\n", c);
+//       
+//       c = (a < b);
+//       printf("a < b: %d\n", c);
+//       
+//       c = (a >= b);
+//       printf("a >= b: %d\n", c);
+//       
+//       c = (a <= b);
+//       printf("a <= b: %d\n", c);
+    return 0;
+}
